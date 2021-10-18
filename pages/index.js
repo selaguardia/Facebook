4baaf1e1df9b71ce8b7ba85bb31ff43ad5da2c99
selaguardia <selaguardia91@gmail.com>
2021-10-18 14:09:14 -0700

    adds fb login but does not work

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import { getSession } from 'next-auth/client'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Login from '../components/Login'
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />;
   return (
     <div>
       <Head>
@@ -15,4 +18,15 @@ export default function Home() {
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  // Get the user
+  const session = await getSession(context);
+  
+  return {
+    props: {
+      session
+    }
+  }
 }
