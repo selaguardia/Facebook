9ae1e0c1b0ccdf44cd007f45e34e2977f8e13b3b
selaguardia <selaguardia91@gmail.com>
2021-10-18 12:32:37 -0700

    left and middle header fixed to one line, adds spacing

--- components/Header.js
+++ components/Header.js
@@ -17,7 +17,7 @@ import HeaderIcon from './HeaderIcon'
 
 function Header() {
   return (
-    <div>
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       {/* Left */}
       <div className="flex items-center">
         <Image 
@@ -29,7 +29,7 @@ function Header() {
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
           <SearchIcon className="h-6 text-gray-600"/>
           <input 
-            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500" 
+            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink" 
             type="text" 
             placeholder="Search Facebook"/>
         </div>
