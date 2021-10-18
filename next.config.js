2383d6f0bc664feba87bd3fdcb18e49d8a3819fe
selaguardia <selaguardia91@gmail.com>
2021-10-18 12:17:38 -0700

    adds fb icon, search bar, and middle header icons

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
+  images: {
+    domains: [
+      "links.papareact.com",
+      "platform-lookaside.fbsbx.com",
+      "firebasestorage.googleapis.com",
+    ],
+  },
   reactStrictMode: true,
-}
+};
+
